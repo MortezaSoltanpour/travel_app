efd8d517cb92321ab850c672d32114c5c50f5532
MortezaSoltanpour <morteza.cs@gmail.com>
2025-12-02 12:40:44 -0500

    Create travel model

---
+++ lib/models/TravelModel.dart
@@ -0,0 +1,21 @@
+class Travelmodel {
+  String name;
+  String description;
+  String imageUrl;
+  String location;
+  String distance;
+  String temp;
+  String rating;
+  String price;
+
+  Travelmodel({
+    required this.name,
+    required this.description,
+    required this.imageUrl,
+    required this.location,
+    required this.distance,
+    required this.temp,
+    required this.rating,
+    required this.price,
+  });
+}
